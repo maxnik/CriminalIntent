d9708175e2b1be0102592d15596cf1abda99c09a
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-11-14 10:33:11 +0200

    Creates an activity for the list of crimes

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by maxnik on 11/14/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
